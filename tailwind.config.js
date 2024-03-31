9976de608318a2a76d2af1829050ca5e6588f4a5
hritesh04 <hritesh04.dev@gmail.com>
2024-03-31 20:47:04 +0530

    project section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        project: "url('/src/assets/images/GEO_SPHERES 03.png')",
+      },
+      boxShadow: {
+        "3xl": "inset 0px 4px 30px 0px rgba(209, 74, 0, 0.1)",
+      },
+    },
   },
   plugins: [],
-}
+};
