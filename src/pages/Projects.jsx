cb11c34a30c7d3e9a3a2796987de5867ecede765
Abhik <theabhik2020@gmail.com>
2024-05-22 23:20:37 +0530

    Final Changes

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -11,13 +11,15 @@ const items = [
     id: 1,
     name: "CampusBot: Your Virtual Guide",
     descripton:
-      "In this project, a Chatbot is developed to assist with admissions, courses, and campus queries. Integrated with UiPath Studio, the bot ensures smooth user interactions, offering timely support and information. Continuous monitoring and enhancements guarantee an efficient user experience.",
+      "In this project, an advanced Chatbot is developed to assist with admissions, courses, and campus queries. Integrated seamlessly with UiPath Studio, the bot ensures smooth user interactions, offering timely support and comprehensive information. Continuous monitoring and iterative enhancements guarantee an efficient and responsive user experience.",
+    photo: require("../assets/images/projects/chatbot.jpg"),
   },
   {
     id: 2,
     name: "Streamlining Candidate Screening with UiPath StudioX",
     descripton:
       "The research utilizes UiPath StudioX to streamline candidate screening by automating resume evaluation and scoring. This system enhances recruitment efficiency by parsing resumes, extracting relevant information, and sending automated acceptance or rejection emails based on predefined criteria.",
+    photo: require("../assets/images/projects/StudioX.jpg"),
   },
 ];
 
@@ -28,18 +30,18 @@ export const MainProject = () => {
       <div
         style={{
           backgroundImage: `url(${background})`,
-         
+
           backgroundAttachment: "fixed",
           backgroundPosition: "center",
           backgroundRepeat: "no-repeat",
           backgroundSize: "cover",
         }}
         id="project_area"
-        className="  w-11/12 mx-auto flex flex-wrap bg-project bg-center bg-contain bg-no-repeat flex items-center mt-20 justify-center"
+        className="  w-11/12 mx-auto flex flex-wrap bg-project bg-center bg-contain bg-no-repeat  items-center mt-20 justify-center"
       >
         <div className="flex flex-wrap mt-12">
           {items.map((item) => (
-            <EventCard  item={item} />
+            <EventCard item={item} />
           ))}
           {/* <div className=" mr-16 h-fit w-fit flex flex-col gap-5 justify-center items-center">
             <div className=" flex items-center justify-center h-fit w-fit">
