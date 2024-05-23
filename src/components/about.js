3fb04fac2606ced1ac17f6fd4cd891c6c8a19afb
hritesh04 <hritesh04.dev@gmail.com>
2024-05-23 15:13:48 +0530

    video autoplay and white box on responsive fix

--- src/components/about.js
+++ src/components/about.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef } from "react";
 import { IoCheckmarkDoneSharp } from "react-icons/io5";
-import UserPic from '../assets/images/why-uipath.jpeg';
-import { motion, useInView, useAnimation } from 'framer-motion';
+import UserPic from "../assets/images/why-uipath.jpeg";
+import { motion, useInView, useAnimation } from "framer-motion";
 import video from "../assets/BUMPER.mp4";
 export default function About() {
   const ref = useRef();
@@ -9,140 +9,163 @@ export default function About() {
   const mainAnimation = useAnimation();
   const videoControl = useRef(null);
 
-
   useEffect(() => {
     if (isInView) {
       mainAnimation.start("visiable");
     }
-    // videoControl?.current?.play();
-  }, [isInView]);
+    if (videoControl.current) {
+      videoControl.current.play();
+    }
+  }, [isInView, mainAnimation]);
   useEffect(() => {
-
     // videoControl?.current?.play();
   }, []);
   return (
-    <section className='py-14'>
+    <section className="py-14">
       <div className="about_wrapper mx-auto w-11/12 py-6">
         <motion.div
           ref={ref}
           variants={{
             hidden: {
-              opacity: 0, y: 20,
+              opacity: 0,
+              y: 20,
             },
             visiable: {
-              opacity: 1, y: 0,
-            }
+              opacity: 1,
+              y: 0,
+            },
           }}
           initial="hidden"
           animate={mainAnimation}
-          transition={{ duration: .5, delay: .25 }}
-          className="heading title text-4xl">
-          Why <span className='highlight'>UiPath?</span>
+          transition={{ duration: 0.5, delay: 0.25 }}
+          className="heading title text-4xl"
+        >
+          Why <span className="highlight">UiPath?</span>
         </motion.div>
-        <div className="wrapper flex flex-wrap-reverse my-5 py-6 gap-4">
+        <div className="wrapper flex flex-wrap-reverse items-center my-5 py-6 gap-4">
           <div className="md:w-6/12 w-full list-none border-0 md:border-r border-r-[#FF7D36] pr-4">
             <motion.div
               ref={ref}
               variants={{
                 hidden: {
-                  opacity: 0, y: 10,
+                  opacity: 0,
+                  y: 10,
                 },
                 visiable: {
-                  opacity: 1, y: 0,
-                }
+                  opacity: 1,
+                  y: 0,
+                },
               }}
               initial="hidden"
               animate={mainAnimation}
-              transition={{ duration: .5, delay: .20 }}
-              className='list'>
-              <IoCheckmarkDoneSharp className='icon' />
-              A vibrant hub within the global network of aspiring minds.
-
+              transition={{ duration: 0.5, delay: 0.2 }}
+              className="list"
+            >
+              <IoCheckmarkDoneSharp className="icon" />A vibrant hub within the
+              global network of aspiring minds.
             </motion.div>
             <motion.div
               ref={ref}
               variants={{
                 hidden: {
-                  opacity: 0, y: 10,
+                  opacity: 0,
+                  y: 10,
                 },
                 visiable: {
-                  opacity: 1, y: 0,
-                }
+                  opacity: 1,
+                  y: 0,
+                },
               }}
               initial="hidden"
               animate={mainAnimation}
-              transition={{ duration: .5, delay: .30 }} className='list'>
-              <IoCheckmarkDoneSharp className='icon' />
+              transition={{ duration: 0.5, delay: 0.3 }}
+              className="list"
+            >
+              <IoCheckmarkDoneSharp className="icon" />
               Dedicated to empowering the next generation of developers.
-
             </motion.div>
             <motion.div
               ref={ref}
               variants={{
                 hidden: {
-                  opacity: 0, y: 10,
+                  opacity: 0,
+                  y: 10,
                 },
                 visiable: {
-                  opacity: 1, y: 0,
-                }
+                  opacity: 1,
+                  y: 0,
+                },
               }}
               initial="hidden"
               animate={mainAnimation}
-              transition={{ duration: .5, delay: .40 }} className='list'>
-              <IoCheckmarkDoneSharp className='icon' />
-              A dynamic collective where technology meets creativity.
-
+              transition={{ duration: 0.5, delay: 0.4 }}
+              className="list"
+            >
+              <IoCheckmarkDoneSharp className="icon" />A dynamic collective
+              where technology meets creativity.
             </motion.div>
             <motion.div
               ref={ref}
               variants={{
                 hidden: {
-                  opacity: 0, y: 10,
+                  opacity: 0,
+                  y: 10,
                 },
                 visiable: {
-                  opacity: 1, y: 0,
-                }
+                  opacity: 1,
+                  y: 0,
+                },
               }}
               initial="hidden"
               animate={mainAnimation}
-              transition={{ duration: .5, delay: .50 }} className='list'>
-              <IoCheckmarkDoneSharp className='icon' />
+              transition={{ duration: 0.5, delay: 0.5 }}
+              className="list"
+            >
+              <IoCheckmarkDoneSharp className="icon" />
               Offers a comprehensive platform for holistic development.
-
             </motion.div>
             <motion.div
               ref={ref}
               variants={{
                 hidden: {
-                  opacity: 0, y: 10,
+                  opacity: 0,
+                  y: 10,
                 },
                 visiable: {
-                  opacity: 1, y: 0,
-                }
+                  opacity: 1,
+                  y: 0,
+                },
               }}
               initial="hidden"
               animate={mainAnimation}
-              transition={{ duration: .5, delay: .60 }} className='list'>
-              <IoCheckmarkDoneSharp className='icon' />
-              Beyond automation, the community encompasses various domains i.e, Mastery in
-
+              transition={{ duration: 0.5, delay: 0.6 }}
+              className="list"
+            >
+              <IoCheckmarkDoneSharp className="icon" />
+              Beyond automation, the community encompasses various domains i.e,
+              Mastery in
             </motion.div>
             <motion.div
               ref={ref}
               variants={{
                 hidden: {
-                  opacity: 0, y: 20,
+                  opacity: 0,
+                  y: 20,
                 },
                 visiable: {
-                  opacity: 1, y: 0,
-                }
+                  opacity: 1,
+                  y: 0,
+                },
               }}
               initial="hidden"
               animate={mainAnimation}
-              transition={{ duration: .5, delay: .70 }} className='list'>
-              <IoCheckmarkDoneSharp className='icon text-3xl' />
-              UiPath's robotic process automation. Exploration of web development, core machine learning, RPA/AI, UI automation, and cloud computing.
-
+              transition={{ duration: 0.5, delay: 0.7 }}
+              className="list"
+            >
+              <IoCheckmarkDoneSharp className="icon text-3xl" />
+              UiPath's robotic process automation. Exploration of web
+              development, core machine learning, RPA/AI, UI automation, and
+              cloud computing.
             </motion.div>
           </div>
           <div className="md:w-5/12 w-full px-4">
@@ -150,23 +173,29 @@ export default function About() {
               ref={ref}
               variants={{
                 hidden: {
-                  opacity: 0, x: 20,
+                  opacity: 0,
+                  x: 20,
                 },
                 visiable: {
-                  opacity: 1, x: 0,
-                }
+                  opacity: 1,
+                  x: 0,
+                },
               }}
               initial="hidden"
               animate={mainAnimation}
-              transition={{ duration: .5, delay: .80 }}
-              className="image rounded-md w-full h-[250px] bg-white">
+              transition={{ duration: 0.5, delay: 0.8 }}
+              className="image rounded-md w-full h-fit bg-white"
+            >
               {/* <img src={UserPic} className='w-full h-full rounded-md' alt="about_img" /> */}
               <video
                 title="Advertisement"
                 webkit-playsinline="true"
                 playsinline="true"
-
-                ref={videoControl} controls loop autoplay="">
+                ref={videoControl}
+                muted
+                loop
+                autoplay=""
+              >
                 <source src={video} type="video/mp4" />
                 You can add additional source tags for different video formats
                 Your browser does not support the video tag.
@@ -178,10 +207,9 @@ export default function About() {
                 <source src={video} type="video/mp4" />
               </video> */}
             </motion.div>
-
           </div>
         </div>
       </div>
     </section>
-  )
+  );
 }
