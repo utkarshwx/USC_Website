71fdfcfa1800bfe8c55a8827930b87cc26e2dc34
Abhik <theabhik2020@gmail.com>
2024-06-30 21:47:44 +0530

    Final Changes

--- src/data/events_data.js
+++ src/data/events_data.js
@@ -1,17 +1,17 @@
-export  const eventsData = [
+export const eventsData = [
   {
     date: "20 JAN",
     name: "ORIENTATION SYNERGY",
     descripton:
       "Anticipation is in the air! Brace yourselves for an evening filled with excitement and delight surprises at our officail meet on Januaray 20th. Get ready to be amazed as we unfold unexpected moments and celebrate the extraordinary together!",
     venue: "CAMPUS 14, SEMINAR HALL",
-    photo:"https://res.cloudinary.com/dy0raom2p/image/upload/v1710099027/dlmzhmrac5ihhiqhh5be.jpg",
+    photo: "https://res.cloudinary.com/dy0raom2p/image/upload/v1710099027/dlmzhmrac5ihhiqhh5be.jpg",
     link: "https://www.instagram.com/p/C2RWUf4hOEZ/?igsh=czE0MmpuaG5wb29z",
   },
   {
     date: "28 JAN",
     name: "RECRUITMENT OF 1ST YEAR",
-    photo:"https://res.cloudinary.com/dy0raom2p/image/upload/v1710151941/lxwbnz6zzyqwxvw5hcj7.jpg",
+    photo: "https://res.cloudinary.com/dy0raom2p/image/upload/v1710151941/lxwbnz6zzyqwxvw5hcj7.jpg",
     descripton:
       "Calling all first-years! Join our dynamic society and experience a year filled with fun events and leadership roles. Grow as a person and professionally, while building your college network. It's the perfect way to shape your college experience!",
     venue: "CAMPUS 3, BLOCK B",
@@ -20,10 +20,28 @@ export  const eventsData = [
   {
     date: "2 MARCH",
     name: "USC KIIT RESEARCH FORUM",
-    photo:require("../assets/images/Events/Reserach_Forum.jpg"),
+    photo: require("../assets/images/Events/Reserach_Forum.jpg"),
     descripton:
       "Get ready to embark on an exciting journey with the inauguration of the USC KIIT Research Forum! Join us as we dive into collaborative exploration, where your ideas ignite and innovations flourish under the guidance of industry-experienced mentors.",
     venue: "USC KIIT",
     link: "https://www.instagram.com/p/C2RWUf4hOEZ/?igsh=czE0MmpuaG5wb29z",
+  },
+  {
+    // date: "2 MARCH",
+    name: "CodeSprint at USC KIIT",
+    photo: require("../assets/images/Events/codesprint.jpg"),
+    descripton:
+      "30 Days of Non-Stop Coding Excellence!Experience the ongoing 30-Day Coding Marathon designed to sharpen problem-solving skills and establish a consistent coding routine! Open to all KIIT students, CodeSprint runs from June 15th to July 14th. Participate, code, and conquer!",
+    venue: "USC KIIT",
+    // link: "https://www.instagram.com/p/C2RWUf4hOEZ/?igsh=czE0MmpuaG5wb29z",
+  },
+  {
+    // date: "2 MARCH",
+    name: "🚀 100 Days Code Challenge at USC KIIT 🚀",
+    photo: require("../assets/images/Events/codesprint.jpg"),
+    descripton:
+      "Celebrating Consistency and Excellence in Coding!Our 100 Days Code Challenge at USC KIIT was a resounding success! From May 20th, 2024, our members engaged in 100 days of non- stop coding, tackling at least 5 codes daily in their favorite languages.",
+    venue: "USC KIIT",
+    // link: "https://www.instagram.com/p/C2RWUf4hOEZ/?igsh=czE0MmpuaG5wb29z",
   },
 ];
