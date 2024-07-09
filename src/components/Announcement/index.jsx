417fe496cc87d8ac5c7ce3618bac2f205feac433
Abhik <theabhik2020@gmail.com>
2024-07-09 08:58:05 +0530

    Final Changes

--- src/components/Announcement/index.jsx
+++ src/components/Announcement/index.jsx
@@ -4,10 +4,10 @@ import ApplyBtn from "./ApplyBtn";
 import Results from "../Results";
 export default function Announcement() {
   return (
-    <div className="w-screen my-5 py-8 min-h-[60vh]">
+    <div className="w-screen my-10 py-8 min-h-[60vh]">
       <div className="wrapper w-11/12 flex mx-auto">
         <div className="w-11/12 text-center">
-          <div className="heading">
+          {/* <div className="heading">
             <span className="italic playwrite-us-trad-annocment text-2xl text-orange-400 ">
               announcement
             </span>
@@ -15,9 +15,9 @@ export default function Announcement() {
               Drive Innovation and Impact: We're Hiring for Tech and Non-Tech
               Domains!
             </h1>
-          </div>
-          <ApplyBtn />
-          <div
+          </div> */}
+          {/* <ApplyBtn /> */}
+          {/* <div
             style={{
               width: "80%",
               margin: "61px auto",
@@ -25,7 +25,7 @@ export default function Announcement() {
               background: "#f5deb347",
             }}
             className="w-full h-[2px] bg-white"
-          ></div>
+          ></div> */}
           <Results/>
         </div>
       </div>
